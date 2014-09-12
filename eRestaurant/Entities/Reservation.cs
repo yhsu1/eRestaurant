1c22396f1a1e3449ab9a27e63b845457cabe1017
yhsu1 <yhsu1@studentmail.nait.ca>
2014-09-11 22:51:04 -0600

    Added Reservation class

--- eRestaurant/Entities/Reservation.cs
+++ eRestaurant/Entities/Reservation.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace eRestaurant.Entities
 {
-    class Reservation
+    public class Reservation
     {
+        public int ReservationID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int NumberParty { get; set; }
+        public string ContactPhone { get; set; }
+        public string ReservationStatus { get; set; }
+        public string EventCode { get; set; }
     }
 }
