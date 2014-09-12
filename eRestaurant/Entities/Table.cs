37fe44f2abf960ae79e7115550be7a49499224f8
yhsu1 <yhsu1@studentmail.nait.ca>
2014-09-11 22:45:04 -0600

    Added table class

---
+++ eRestaurant/Entities/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    public class Table
+    {
+        // By convention, if we use a property with the same name at the class, but
+        // with a suffix of ID, then Entity Framework will recognize that property
+        // as mapping to the database table's Primary key column.
+        public int TableID { get; set; }
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+    }
+}
